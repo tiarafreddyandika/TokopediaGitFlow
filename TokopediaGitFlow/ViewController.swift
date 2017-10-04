a2551abe9d4474029fbceb86ff6aa56d9e50770d
Tiara Freddy Andika <tiara.andika@tokopedia.com>
2017-10-04 14:32:11 +0700

    Create Project

---
+++ TokopediaGitFlow/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  TokopediaGitFlow
+//
+//  Created by Tiara Freddy Andika on 04/10/17.
+//  Copyright © 2017 Tiara Freddy Andika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    /*
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    */
+    /*
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    */
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
